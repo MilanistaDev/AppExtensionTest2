4f6f4e477c3d18ae8440f3cafd1e8165fc300896
MilanistaDev <milanista224@gmail.com>
2015-03-09 16:33:42 +0900

    Initial Commit

---
+++ AppExtensionTest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppExtensionTest2
+//
+//  Created by 麻生 拓弥 on 2015/03/09.
+//  Copyright (c) 2015年 麻生 拓弥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
